e97742eb8a6b10d6e08e18e8bdba7e4072d240bd
siddhantahireofficial <siddhantahireofficial@gmail.com>
2021-06-25 22:38:00 +0530

    search bar api created

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {create, productById, read, remove,update,list,listRelated,listCategories,listBySearch,photo} = require('../controllers/product');
+const {create, productById, read, remove,update,list,listRelated,listCategories,listBySearch,photo, listSearch } = require('../controllers/product');
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 
 router.get('/product/:productId',read);
+router.get('/products/search',listSearch);
 router.delete('/product/:productId/:userId',requireSignin,isAuth,isAdmin,remove);
 router.put('/product/:productId/:userId',requireSignin,isAuth,isAdmin,update);
 router.post('/product/create/:userId',requireSignin,isAuth,isAdmin,create);
